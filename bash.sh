3a9f8d250b1d8331b9c638ddfae268c89d7385ff
Sara <sara.rey.manchester@gmail.com>
2019-05-12 20:06:34 +0100

    Addition to call python script

--- bash.sh
+++ bash.sh
@@ -1,2 +1,4 @@
 > Desktop/all_files.txt; for i in $(ls Desktop/tmp/*gene*); do echo $i | cut -d'_' -f5 >> Desktop/all_files.txt; done
 cat Desktop/all_files.txt | sort | uniq > Desktop/unique_files.txt
+
+for i in $(cat unique_files.txt); do echo $i; python coverageComp/coverage_comp.py tmp/*"-CCBPG"*$i"gene" tmp/*"TAFXY"*$i"gene"; done
