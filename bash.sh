429c366d23c2f52598e43532ef4bb6fa5dc75b96
Sara Rey <sara.rey.manchester@gmail.com>
2019-05-13 11:41:14 +0100

    completed

--- bash.sh
+++ bash.sh
@@ -3,5 +3,8 @@ cat Desktop/all_files.txt | sort | uniq > Desktop/unique_files.txt
 
 for i in $(cat unique_files.txt); do echo $i; python coverageComp/coverage_comp.py tmp/*"-CCBPG"*$i"gene" tmp/*"TAFXY"*$i"gene"; done
 
+for i in $(cat ../unique_files.txt); do echo $i; python coverage_comp.py ../tmp/*"-CCBPG"*$i"gene" ../tmp/*"TAFXY"*$i"gene"; done
+
+for i in $(cat ../unique_files.txt); do echo $i; miseq=$(find .. -maxdepth 3 -name *"-CCBPG"*$i*"gene"*); nextseq=$(find .. -maxdepth 3 -name *"TAFXY"*$i*"gene"*); python coverage_comp.py $miseq $nextseq ; done
 
 
--- coverageComp/coverage_comp.py
+++ coverageComp/coverage_comp.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-orig_path = "/Users/sararey/Desktop/tmp/"
-orig_path = "C:\\Users\\saram\\Documents\\Work\\analysis\\tmp"
+orig_path = "../tmp/"
 pth = os.path.join(orig_path)
 outpath = os.path.join( os.path.dirname( __file__ ), '..' )
 
@@ -15,19 +14,20 @@ for i in dir_list:
         print(i.split("_")[4])
 '''
 
-miseq_cov = "190326_M02641_0195_000000000-CCBPG_11M01274_gene_coverage" #sys.argv[1]
-nextseq_cov = "190415_NB551415_0015_AHKMFTAFXY_11M01274_gene_coverage" #sys.argv[2]
-
-outfile_name = nextseq_cov.split("_")[4] + ".tsv"
-#print(outfile_name)
+miseq_cov =  sys.argv[1]
+nextseq_cov = sys.argv[2]
 
 miseq_cov_path = os.path.join(pth, miseq_cov)
 nextseq_cov_path = os.path.join(pth, nextseq_cov)
+
+outfile_name = nextseq_cov_path.split("_")[4] + ".tsv"
+#print(outfile_name)
+
 outfile = os.path.join(outpath, outfile_name)
 
 with open(miseq_cov_path) as msc, open(nextseq_cov_path) as nsc, open (outfile, "w") as of:
         for x,y in zip(msc,nsc):
-            of.write(x.strip() + "," + "," + y.strip() + "\n")
+            of.write(x.strip() + "\t" + "\t" + y.strip() + "\n")
         print("Writing file " + outfile_name)
 
 
