6e6e2c37ec8cdfe0d14a13aea0836342c6d2f015
Sara Rey <sara.rey.manchester@gmail.com>
2019-05-15 17:31:43 +0100

    changes to update vcf

--- remove_lowq_gts/remove_lowq_gts.py
+++ remove_lowq_gts/remove_lowq_gts.py
@@ -21,6 +21,7 @@ with open(vcf) as vf, open(outfile , "w") as of:
                         genotype_field[0] = "./."
                         sample = ":".join(genotype_field)
                         current_line[9+y] = sample
-                print(current_line)
-        of.writelines(current_line)
+                #print(current_line)
+        #print("\t".join(current_line))
+        of.write("\t".join(current_line))
         of.write("\n")
